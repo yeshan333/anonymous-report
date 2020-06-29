540124ab91775ffa8b1e858ad3184a01620ad53f
yeshan333 <1329441308@qq.com>
2020-06-29 20:55:23 +0800

    更新服务端数据接口

--- src/services/getsinglerecords.ts
+++ src/services/getsinglerecords.ts
@@ -1,11 +1,11 @@
 /*
-  * 获取最近上传的记录
-*/
+ * 获取最近上传的记录
+ */
 
-import request  from 'umi-request';
+import request from 'umi-request';
 
-const URL = 'http://47.92.4.141:8080/api/user/findLatestInf';
+const URL = 'http://47.92.4.141:8080/api/findLatestInf/admin';
 
 export async function getSingleRecords() {
-    return request(URL);
+  return request(URL);
 }
